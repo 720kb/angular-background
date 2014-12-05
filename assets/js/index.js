178724d0337207fefafa08cbe6d67807ee0cf858
45kb <filippo.oretti@gmail.com>
2014-12-05 09:47:01 +0100

    need to do index.html and README

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,17 @@
 /*global angular*/
 
-(function (angular) {
+(function withAngular(angular) {
   'use strict';
 
   angular.module('720kb', [
     'ngRoute',
-    '720kb.background'
-  ]);
+    '720kb.background',
+    'hljs'
+  ]).config(['hljsServiceProvider', function configurationFunction(hljsServiceProvider) {
+
+    hljsServiceProvider.setOptions({
+      // replace tab with 4 spaces
+      'tabReplace': ''
+    });
+  }]);
 }(angular));
